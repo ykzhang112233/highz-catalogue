939325da7635ad366a49f6d38af440e10d6ef0dc
“ykzhang”112233 <ykzhang@shao.ac.cn>
2025-07-16 17:54:54 +0800

    replace null values for recognizable nans in python

--- scripts/make_cat.py
+++ scripts/make_cat.py
@@ -1,6 +1,7 @@
 # Just some utility functions to make the output catalogue more hunman-readable.
 import pandas as pd
 from astropy.io import fits
+import numpy as np
 
 
 # read the fits file and convert it to a Pandas DataFrame
@@ -24,7 +25,7 @@ new_columns = [
     "Fpk_racs", "e_Fpk_racs", "Ftot_racs", "e_Ftot_racs"
 ]
 df.rename(columns={old: new for old, new in zip(columns, new_columns)}, inplace=True)
-
+df.replace(-32768, np.nan, inplace=True)
 df.to_csv('../final_matched_highz_catalogue.csv', index=False)
 
 #Usage example:
